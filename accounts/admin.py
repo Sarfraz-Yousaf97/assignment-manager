bf214bffcecbafc96bf76f9a20d251b8fcb59f0f
Sarfraz-Yousaf97 <muhammadsarfrazyousaf009@gmail.com>
2025-08-20 14:31:36 +0500

    initalize admin models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User
+from .models import User, Project, ProjectRole, Task
 
 
 admin.site.register(User)
+admin.site.register(Task)
+admin.site.register(ProjectRole)
+admin.site.register(ProjectRole)
